c559cf6989a570b81e70d694cbfa639cd249c731
Aimen Farooq <aimen.fq@gmail.com>
2026-04-27 10:45:03 -0500

    Add files via upload

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&10^c26@b9t)xg8)0+(so-ye98m_wfj$wasv&36s%yucwgt+dy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
