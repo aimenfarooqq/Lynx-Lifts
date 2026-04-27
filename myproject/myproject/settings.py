d3f177171c7e11daaf7e410b4c58026971bd5463
Aimen Farooq <aimen.fq@gmail.com>
2026-04-27 15:37:46 -0500

    Add files via upload

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -75,11 +75,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
