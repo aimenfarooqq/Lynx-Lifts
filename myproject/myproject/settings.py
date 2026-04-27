56dcb4c436c105a281cae8cf334d187d1ce1964c
Aimen Farooq <aimen.fq@gmail.com>
2026-04-27 18:16:30 -0500

    Add files via upload

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -82,3 +82,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ['https://lynx-lifts-production.up.railway.app']
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
