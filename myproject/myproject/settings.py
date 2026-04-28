f058e02d9d151ea1c45e3b3fb4e127ce4c3cee7f
Aimen Farooq <aimen.fq@gmail.com>
2026-04-28 00:05:36 -0500

    Add files via upload

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -84,7 +84,8 @@ CSRF_TRUSTED_ORIGINS = ['https://lynx-lifts-production.up.railway.app']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
